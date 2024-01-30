0cecada1267f842b459745b535b639c6e311090f
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-01-30 11:11:30 +0100

    Navigation ok with routes ok

--- app/(tabs)/Ads/Ads_List.js
+++ app/(tabs)/Ads/Ads_List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native-web';
+import { View, Text } from 'react-native';
 
 const Ads_List = () => {
     return (
--- app/(tabs)/Ads/Research.js
+++ app/(tabs)/Ads/Research.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native-web';
+import { View, Text } from 'react-native';  // Utilisez l'importation de 'react-native'
 
 const Research = () => {
     return (
