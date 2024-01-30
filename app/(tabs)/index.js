451518998ff32fcca132e9d21365187ba03af77d
Marilyn Olejniczak <92530551+ZyanneM@users.noreply.github.com>
2024-01-30 17:20:33 +0100

    Fix conflict index.js

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -2,20 +2,19 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginProvider from "../../context/LoginProvider";
-import WelcomeNavigator from "./navigators/WelcomeNavigators"; // Importer depuis le bon chemin
+import WelcomeNavigator from "./navigators/WelcomeNavigators";
 import AuthNavigator from "./navigators/AuthNavigator";
-import MenuNavigator from "./navigators/MenuNavigator";
-import MenuContainer from "../../components/Menu/MenuContainer";
+import MenuNavigator from "./navigators/MenuNavigator"; // Adjust the path as needed
 import UserNavigators from "./navigators/UserNavigators";
+import MenuContainer from "../../components/Menu/MenuContainer";
 
 const Stack = createNativeStackNavigator();
-
 
 const App = () => {
   return (
     <LoginProvider>
       <NavigationContainer independent={true}>
-      <MenuContainer />
+        <MenuContainer />
         <Stack.Navigator headerMode="none">
           <Stack.Screen
             name="Welcome"
@@ -27,14 +26,14 @@ const App = () => {
             component={AuthNavigator}
             options={{ headerShown: false }}
           />
-          {/* <Stack.Screen
-            name="Users"
-            component={UserNavigators}
-            options={{ headerShown: false }}
-          /> */}
           <Stack.Screen
             name="Menu"
             component={MenuNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Users"
+            component={UserNavigators}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
